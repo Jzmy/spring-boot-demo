5fe271334bb667df6c9292d52e4d34d98db37999
Jzmy <70988208+Jzmy@users.noreply.github.com>
2022-06-01 09:37:39 +0800

    加密

---
+++ spring-boot-duing-demo/spring-boot-duing-02-configuration/src/main/java/com/duing/util/JasyptUtil.java
@@ -0,0 +1,15 @@
+package com.duing.util;
+
+import org.jasypt.util.text.BasicTextEncryptor;
+
+public class JasyptUtil {
+
+    public static void main(String[] args) {
+        BasicTextEncryptor basicTextEncryptor = new BasicTextEncryptor();
+        basicTextEncryptor.setPassword("123456!@#");
+       String username =  basicTextEncryptor.encrypt("root");
+       String password = basicTextEncryptor.encrypt("root123");
+        System.out.println(username);
+        System.out.println(password);
+    }
+}
